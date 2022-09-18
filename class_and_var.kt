48c4c24d541584d34c9ffe54b0f4977041289ca2
Kumar Purushottam <88957070+KumarPurushottam107@users.noreply.github.com>
2022-09-19 01:52:48 +0530

    Create class_and_var.kt

---
+++ class_and_var.kt
@@ -0,0 +1,15 @@
+class Galexy
+{
+    var body : String = ""; 
+}
+
+
+fun main() 
+{
+    var planet =  Galexy()
+    
+    planet.body = "Earth"
+    
+    println("The name of the planet is ${planet.body}")
+    
+}
