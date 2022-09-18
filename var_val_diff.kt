dfb123f65b0d89eb536b2e2f7da2c73e2db36180
Kumar Purushottam <88957070+KumarPurushottam107@users.noreply.github.com>
2022-09-19 01:57:07 +0530

    Create var_val_diff.kt

---
+++ var_val_diff.kt
@@ -0,0 +1,8 @@
+fun main() 
+{
+   var marks = 76 // Marks for a student can change so it is kept in var. 
+   val rollNo = 107 // RollNo for a student can't be changed so it kept in val 
+    
+   println("Roll NO ${rollNo} got ${marks} marks") 
+    
+}
