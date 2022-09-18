52b05a209b2e191f7c81b8c3168eeb445ec4fe7d
Kumar Purushottam <88957070+KumarPurushottam107@users.noreply.github.com>
2022-09-19 02:26:38 +0530

    Update if_else.kt

--- if_else.kt
+++ if_else.kt
@@ -7,12 +7,8 @@ fun main()
     //num1 = 10
     //num2 = 10
     
-    var result = if(num1>num2)
-    				num1
-    			else if(num1<num2)
-                	num2
-    			else 
-    				0
+   var result = if(num1>num2) num1 else if(num1<num2) num2 else 0
+ 
     println(result);
     
     
