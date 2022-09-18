158baee0a69c4d931f8b5e8d4e15edac78f4450a
Kumar Purushottam <88957070+KumarPurushottam107@users.noreply.github.com>
2022-09-19 02:21:18 +0530

    Create if_else.kt

---
+++ if_else.kt
@@ -0,0 +1,19 @@
+fun main() 
+{
+ 	
+    var num1 : Int = 4 ; 
+    var num2 : Int = 3 ; 
+    
+    //num1 = 10
+    //num2 = 10
+    
+    var result = if(num1>num2)
+    				num1
+    			else if(num1<num2)
+                	num2
+    			else 
+    				0
+    println(result);
+    
+    
+}
