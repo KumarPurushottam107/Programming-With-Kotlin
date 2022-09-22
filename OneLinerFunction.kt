f0d53c3af0c237e353680643dccfabd04218a3f2
Kumar Purushottam <88957070+KumarPurushottam107@users.noreply.github.com>
2022-09-22 10:13:49 +0530

    Create OneLinerFunction.kt

---
+++ OneLinerFunction.kt
@@ -0,0 +1,32 @@
+fun main() 
+{
+	
+    // Functional Expression 
+    
+    var sum1 = add(4,5)
+    
+    println("Sum1 is $sum1")
+    
+   var sum2 = oneLineSum(8,11)
+    
+   println("Sum2 is $sum2")
+     
+   var sum3 = oneLineMax(5,6) 
+   
+   println("Max is $sum3")
+   
+    
+}
+
+
+fun add(a : Int, b : Int) : Int
+{
+    return(a+b)
+}
+
+// One Linear Function
+
+fun oneLineSum( a : Int , b : Int) = a+b
+
+fun oneLineMax( a: Int , b : Int ) = if(a>b) a else b 
+
