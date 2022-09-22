a8df4b5f6e19a30e5427c55c19b97c5a38ff737d
Kumar Purushottam <88957070+KumarPurushottam107@users.noreply.github.com>
2022-09-22 07:32:41 +0530

    Create while_loop.kt

---
+++ while_loop.kt
@@ -0,0 +1,19 @@
+fun main() 
+{
+    
+    // while syntax 
+    
+    var limit : Int = 3 
+    
+    while(limit>0)
+    {
+        println("Hare Rama Here Rama Here Krishna Here Krishna")
+        println()
+        
+        limit--
+        
+    }
+    
+    
+    
+}
