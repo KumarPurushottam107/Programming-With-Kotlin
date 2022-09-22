3317a4096e2927276ee3763e5432a30c4b76d7b9
Kumar Purushottam <88957070+KumarPurushottam107@users.noreply.github.com>
2022-09-22 06:49:21 +0530

    Create when_in_Kotlin.kt

---
+++ when_in_Kotlin.kt
@@ -0,0 +1,23 @@
+fun main() {
+    
+   // when expression :- It is similar to switch state in c++, Java
+   
+   var name : String  = "Avinash" 
+    
+    when(name)
+    {
+        "Amri" -> println("Dedication and Smartness")
+        
+        "Avinash" -> println("Symbol of Power")
+        
+        "Ashutosh" -> println("Sharp and Humorous")
+        
+    }
+    
+    // Output will be Symbol of Power
+    
+    
+    
+   
+   
+}
